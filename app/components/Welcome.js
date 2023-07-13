4ff3938cce2cdde6aa1322149c05d837cb106bef
Ghanshyam-03 <gsrajput2002@gmail.com>
2023-07-13 10:41:41 +0530

    welcome page

--- app/components/Welcome.js
+++ app/components/Welcome.js
@@ -15,13 +15,13 @@ function Welcome(){
           <div className='text-5xl text-white font-bold pb-5'>Let's democratize AI together</div>
           <span className='text-2xl text-white'>Unleash the potential of AI in research. Discover insights, fuel innovation, and shape a transformative future with our cutting-edge tools and technologies</span>
           <div className='flex items-center justify-center mt-5'>
-          <a href="https://dreamqr.art/">
+          <a href="https://dreamqr.art/" target='_blank'>
             <button className="flex text-xl bg-[#3a289f] font-bold p-3 px-9  m-6 border-2 text-xl rounded-lg justify-center items-center">
               <span>Try Dream-QR</span>
               
             </button>
           </a>
-          <a href="https://dreamqr.art/">
+          <a href="https://chatbrowser.io/" target='_blank'>
             <button className="flex text-xl bg-[#7964F6] font-bold p-3 px-9  m-6 border-2 text-xl rounded-lg justify-center items-center">
               <span>Try Chat-Browser</span>
               
